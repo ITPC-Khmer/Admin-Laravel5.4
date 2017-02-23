07cd2197a747e048464af63a66d5ff690170160c
ITPC-Khmer <ksmart.lion@gmail.com>
2017-02-23 08:43:39 +0700

    update note

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Mail\OrderShipped;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Mail;
 
 class PageController extends Controller
 {
     // for list + page
     function index()
     {
+       Mail::to('ksmart.lion@gmail.com')
+           ->send(new OrderShipped());
        return view('admin.page.index');
     }
 
---
+++ app/Mail/OrderShipped.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class OrderShipped extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.orders_shipped')
+            ->subject('test lion')
+            ->attach('lion.jpg')
+            ->attach('Laravel5.4.pdf');
+    }
+}
